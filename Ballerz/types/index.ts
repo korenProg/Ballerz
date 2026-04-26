2c2247d6eb3f98f04808ffbdef3a0c797ef26d7c
korenShemer <korensh2149@gmail.com>
2026-04-26 16:32:33 +0300

    feat: add League type

--- Ballerz/types/index.ts
+++ Ballerz/types/index.ts
@@ -1,2 +1,3 @@
 export * from './games';
 export * from './players';
+export type { League } from "./league";
---
+++ Ballerz/types/league.ts
@@ -0,0 +1,8 @@
+export type League = {
+  name: string;
+  logoUri: string | null;
+  color: string;
+  defaultLocation: string;
+  defaultTeamSize: number;
+  adminName: string;
+};
